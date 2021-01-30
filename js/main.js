df7b73dbec14f332be65ecdd149ca951686182de
Abdo M <abdelrahman_mohey@yahoo.com>
2021-01-30 04:36:31 +0200

    make if condition and timer working

--- js/main.js
+++ js/main.js
@@ -6,8 +6,20 @@ let myDate = new Date().getTime();
 
 let testfun = (e) =>{
     e.preventDefault();
-    $lineReminder.textContent = `The Event is: ${$titleInput.value} & ${$dateInput.value} & ${myDate}` ;
+
+    let dateWritten = Date.parse($dateInput.value);
+    let dateDiff = dateWritten - myDate;
+    let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+    let cond1 = $titleInput.value !== '' && $dateInput.value !== '' && dateDiff >= 0 ? $lineReminder.textContent = `The Event is: ${$titleInput.value}. The remaining time is:  ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} Sec` 
+    :dateDiff < 0 ? alert("The event is now")
+    :$lineReminder.textContent = `Please fill the inputs`
 }
+
+//create p element
+
 $btn.addEventListener('click', testfun)
 
 /*let dateWritten = new Date($dateInput).getTime();
